e67ffdb6a2c05a448cf594ee9845d14d055b4f39
Derek Elkins <derek.a.elkins@gmail.com>
2017-08-11 20:12:06 -0700

    Add tsconfig.json and generator fetches.

--- rel.ts
+++ rel.ts
@@ -13,10 +13,11 @@ interface Map<V> {
 interface PathSpec extends Map<PathSpec> {}
 
 export default class Table<V extends Map<any>> {
+    private readonly rows: Array<V>;
     private readonly indexes: Map<Map<Table<V>>>;
 
     private static makePathSpec(indexSpec: Array<Array<string>>): PathSpec {
-        const pathSpec = {};
+        const pathSpec: PathSpec = {};
         indexSpec.forEach(spec => {
             let current = pathSpec;
             spec.forEach(field => {
@@ -35,7 +36,7 @@ export default class Table<V extends Map<any>> {
             let index = this.indexes[k];
             if(index === undefined) this.indexes[k] = index = {};
             let subTable = index[val];
-            if(subTable === undefined) index[val] = subTable = new Table([], this.pathSpec[k]);
+            if(subTable === undefined) index[val] = subTable = new Table<V>(this.pathSpec[k]);
             subTable.insert(row);
         }
     }
@@ -44,7 +45,7 @@ export default class Table<V extends Map<any>> {
         if(indexSpec === undefined) indexSpec = [];
         // TODO: Assert no duplicates in each subarray.
         const pathSpec = Table.makePathSpec(indexSpec);
-        const t = new Table([], pathSpec);
+        const t = new Table<V>(pathSpec);
         const rowLen = rows.length;
         for(let i = 0; i < rowLen; ++i) {
             t.insert(rows[i]);
@@ -52,11 +53,12 @@ export default class Table<V extends Map<any>> {
         return t;
     }
 
-    private constructor(private readonly rows: Array<V>, private readonly pathSpec: PathSpec) { 
+    private constructor(private readonly pathSpec: PathSpec) { 
+        this.rows = [];
         this.indexes = {};
     }
 
-    fetchWhere(predicate: (v:any) => boolean, ...keys: Array<[string, V]>) : Array<V> {
+    *fetchWhereGen(predicate: (v: any) => boolean, ...keys: Array<[string, V]>): Iterable<V> {
         const keyLen = keys.length;
 
         let data: Table<V> = this;
@@ -65,7 +67,63 @@ export default class Table<V extends Map<any>> {
         for(; i < keyLen; ++i) {
             const [key, val] = keys[i];
             const index = data.indexes[key];
-            if(index === void(0)) break;
+            if(index === undefined) break;
+            data = index[val.toString()];
+        }
+
+        if (i < keyLen) {
+            for(let row of data.rows) {
+                let j = i;
+                for(; j < keyLen; j++) {
+                    const [key, val] = keys[j];
+                    const col = row[key];
+                    if(col !== val || !predicate(col)) break;
+                }
+                if (j >= keyLen) yield row;
+            }
+        } else {
+            yield* data.rows;
+        }
+    }
+
+    *fetchGen(...keys: Array<[string, any]>): Iterable<V> {
+        const keyLen = keys.length;
+
+        let data: Table<V> = this;
+        let i: number = 0;
+
+        for(; i < keyLen; ++i) {
+            const [key, val] = keys[i];
+            const index = data.indexes[key];
+            if(index === undefined) break;
+            data = index[val.toString()];
+        }
+
+        if (i < keyLen) {
+            for(let row of data.rows) {
+                let j = i;
+                for(; j < keyLen; j++) {
+                    const [key, val] = keys[j];
+                    const col = row[key];
+                    if(col !== val) break;
+                }
+                if (j >= keyLen) yield row;
+            }
+        } else {
+            yield* data.rows;
+        }
+    }
+
+    fetchWhere(predicate: (v: any) => boolean, ...keys: Array<[string, V]>) : Array<V> {
+        const keyLen = keys.length;
+
+        let data: Table<V> = this;
+        let i: number = 0;
+
+        for(; i < keyLen; ++i) {
+            const [key, val] = keys[i];
+            const index = data.indexes[key];
+            if(index === undefined) break;
             data = index[val.toString()];
         }
 
@@ -92,7 +150,7 @@ export default class Table<V extends Map<any>> {
         for(; i < keyLen; ++i) {
             const [key, val] = keys[i];
             const index = data.indexes[key];
-            if(index === void(0)) break;
+            if(index === undefined) break;
             data = index[val.toString()];
         }
 
@@ -115,6 +173,6 @@ const indexSpec = [['c'],['a','b']];
 const t = Table.create([{a:1, b:1, c:1},{a:1,b:2,c:3},{a:2,b:1,c:4}], indexSpec);
 
 console.log(t);
-//console.log(t.fetch());
+console.log(t.fetch());
 console.log(t.fetch(['b',1]));
-//console.log(t.fetch(['a',1],['b',1]));
+console.log(t.fetch(['a',1],['b',1]));
