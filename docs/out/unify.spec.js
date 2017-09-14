0616cd8a0d374d0482de5a93cbc7a754a523158f
Derek Elkins <derek.a.elkins@gmail.com>
2017-09-13 18:32:13 -0700

    Add tests for groundJson, groundJsonNoSharing, refreshJson.

--- docs/out/unify.spec.js
+++ docs/out/unify.spec.js
@@ -27,6 +27,55 @@ var __read = (this && this.__read) || function (o, n) {
     Object.defineProperty(exports, "__esModule", { value: true });
     require("jest");
     var unify_1 = require("./unify");
+    test('groundJsonNoSharing', function () {
+        var _a = __read(unify_1.Substitution.emptyPersistent().fresh(7), 2), _b = __read(_a[0], 7), X = _b[0], Y = _b[1], Z = _b[2], W = _b[3], A = _b[4], B = _b[5], U = _b[6], sub = _a[1];
+        var s = sub.bind(X, { foo: 1, bar: void (0), baz: [1, 2], quux: {} });
+        s = s.bind(B, 1);
+        s = s.bind(Y, [1, void (0), [1, 2], {}]);
+        s = s.bind(Z, { foo: 1, bar: void (0), baz: [1, 2], quux: {}, objVar: X, arrVar: Y, primVar: B, unboundVar: U });
+        s = s.bind(W, [1, void (0), [1, 2], {}, X, Y, B, U]);
+        s = s.bind(A, [W, Z]);
+        expect(unify_1.groundJsonNoSharing(A, s)).toEqual([
+            [1, void (0), [1, 2], {}, { foo: 1, bar: void (0), baz: [1, 2], quux: {} }, [1, void (0), [1, 2], {}], 1, U],
+            { foo: 1, bar: void (0), baz: [1, 2], quux: {}, objVar: { foo: 1, bar: void (0), baz: [1, 2], quux: {} }, arrVar: [1, void (0), [1, 2], {}], primVar: 1, unboundVar: U }
+        ]);
+    });
+    test('groundJson', function () {
+        var _a = __read(unify_1.Substitution.emptyPersistent().fresh(7), 2), _b = __read(_a[0], 7), X = _b[0], Y = _b[1], Z = _b[2], W = _b[3], A = _b[4], B = _b[5], U = _b[6], sub = _a[1];
+        var s = sub.bind(X, { foo: 1, bar: void (0), baz: [1, 2], quux: {} });
+        s = s.bind(B, 1);
+        s = s.bind(Y, [1, void (0), [1, 2], {}]);
+        s = s.bind(Z, { foo: 1, bar: void (0), baz: [1, 2], quux: {}, objVar: X, arrVar: Y, primVar: B, unboundVar: U });
+        s = s.bind(W, [1, void (0), [1, 2], {}, X, Y, B, U]);
+        s = s.bind(A, [W, Z]);
+        expect(unify_1.groundJson(A, s)).toEqual([
+            [1, void (0), [1, 2], {}, { foo: 1, bar: void (0), baz: [1, 2], quux: {} }, [1, void (0), [1, 2], {}], 1, U],
+            { foo: 1, bar: void (0), baz: [1, 2], quux: {}, objVar: { foo: 1, bar: void (0), baz: [1, 2], quux: {} }, arrVar: [1, void (0), [1, 2], {}], primVar: 1, unboundVar: U }
+        ]);
+    });
+    test('refreshJson simple linear', function () {
+        var _a = __read(unify_1.Substitution.emptyPersistent().fresh(4), 2), _b = __read(_a[0], 4), X = _b[0], Y = _b[1], Z = _b[2], W = _b[3], sub = _a[1];
+        var _c = __read(unify_1.refreshJson([Z, W], sub), 2), t = _c[0], subResult = _c[1];
+        var _d = __read(subResult.freshVar(), 2), v = _d[0], _ = _d[1];
+        expect([t, v]).toEqual([
+            [new unify_1.Variable(4), new unify_1.Variable(5)],
+            new unify_1.Variable(6)
+        ]);
+    });
+    test('refreshJson simple non-linear', function () {
+        var _a = __read(unify_1.Substitution.emptyPersistent().fresh(4), 2), _b = __read(_a[0], 4), X = _b[0], Y = _b[1], Z = _b[2], W = _b[3], sub = _a[1];
+        var _c = __read(unify_1.refreshJson([Z, Z], sub), 2), t = _c[0], subResult = _c[1];
+        var _d = __read(subResult.freshVar(), 2), v = _d[0], _ = _d[1];
+        expect([t, v]).toEqual([
+            [new unify_1.Variable(4), new unify_1.Variable(4)],
+            new unify_1.Variable(5)
+        ]);
+    });
+    test('lookup undefined', function () {
+        var _a = __read(unify_1.Substitution.emptyPersistent().freshVar(), 2), X = _a[0], sub = _a[1];
+        var s = sub.bind(X, void (0));
+        expect(s.lookupAsVar(X)).toBe(void (0));
+    });
     test('simple lookup unbound', function () {
         var _a = __read(unify_1.Substitution.emptyPersistent().freshVar(), 2), X = _a[0], sub = _a[1];
         expect(typeof sub.lookup(X)).toBe('number');
--- unify.spec.ts
+++ unify.spec.ts
@@ -1,5 +1,66 @@
 import "jest"
-import { Variable, Substitution } from "./unify"
+import { Variable, Substitution,
+         groundJsonNoSharing, groundJson, refreshJson } from "./unify"
+
+// TODO: Add tests for the *Json functions:
+//export function looseMatchJson(x: JsonTerm, y: Json, sub: Substitution<JsonTerm>): Substitution<JsonTerm> | null {
+//export function matchJson(x: JsonTerm, y: Json, sub: Substitution<JsonTerm>): Substitution<JsonTerm> | null {
+//export function looseUnifyJson(x: JsonTerm, y: JsonTerm, sub: Substitution<JsonTerm>): Substitution<JsonTerm> | null {
+//export function unifyJson(x: JsonTerm, y: JsonTerm, sub: Substitution<JsonTerm>): Substitution<JsonTerm> | null {
+
+test('groundJsonNoSharing', () => {
+    const [[X, Y, Z, W, A, B, U], sub] = Substitution.emptyPersistent<any>().fresh(7);
+    let s = sub.bind(X, {foo: 1, bar: void(0), baz: [1,2], quux: {} });
+    s = s.bind(B, 1);
+    s = s.bind(Y, [1, void(0), [1,2], {}]);
+    s = s.bind(Z, {foo: 1, bar: void(0), baz: [1,2], quux: {}, objVar: X, arrVar: Y, primVar: B, unboundVar: U });
+    s = s.bind(W, [1, void(0), [1,2], {}, X, Y, B, U]);
+    s = s.bind(A, [W, Z]);
+    expect(groundJsonNoSharing(A, s)).toEqual([
+        [1, void(0), [1,2], {}, {foo: 1, bar: void(0), baz: [1,2], quux: {} }, [1, void(0), [1,2], {}], 1, U]
+        ,{foo: 1, bar: void(0), baz: [1,2], quux: {}, objVar: {foo: 1, bar: void(0), baz: [1,2], quux: {} }, arrVar: [1, void(0), [1,2], {}], primVar: 1, unboundVar: U }
+    ]);
+});
+
+test('groundJson', () => {
+    const [[X, Y, Z, W, A, B, U], sub] = Substitution.emptyPersistent<any>().fresh(7);
+    let s = sub.bind(X, {foo: 1, bar: void(0), baz: [1,2], quux: {} });
+    s = s.bind(B, 1);
+    s = s.bind(Y, [1, void(0), [1,2], {}]);
+    s = s.bind(Z, {foo: 1, bar: void(0), baz: [1,2], quux: {}, objVar: X, arrVar: Y, primVar: B, unboundVar: U });
+    s = s.bind(W, [1, void(0), [1,2], {}, X, Y, B, U]);
+    s = s.bind(A, [W, Z]);
+    expect(groundJson(A, s)).toEqual([
+        [1, void(0), [1,2], {}, {foo: 1, bar: void(0), baz: [1,2], quux: {} }, [1, void(0), [1,2], {}], 1, U]
+        ,{foo: 1, bar: void(0), baz: [1,2], quux: {}, objVar: {foo: 1, bar: void(0), baz: [1,2], quux: {} }, arrVar: [1, void(0), [1,2], {}], primVar: 1, unboundVar: U }
+    ]);
+});
+
+test('refreshJson simple linear', () => {
+    const [[X, Y, Z, W], sub] = Substitution.emptyPersistent<any>().fresh(4);
+    const [t, subResult] = refreshJson([Z, W], sub);
+    const [v, _] = subResult.freshVar();
+    expect([t, v]).toEqual([
+        [new Variable(4), new Variable(5)],
+        new Variable(6)
+    ]);
+});
+
+test('refreshJson simple non-linear', () => {
+    const [[X, Y, Z, W], sub] = Substitution.emptyPersistent<any>().fresh(4);
+    const [t, subResult] = refreshJson([Z, Z], sub);
+    const [v, _] = subResult.freshVar();
+    expect([t, v]).toEqual([
+        [new Variable(4), new Variable(4)],
+        new Variable(5)
+    ]);
+});
+
+test('lookup undefined', () => {
+    const [X, sub] = Substitution.emptyPersistent<any>().freshVar();
+    const s = sub.bind(X, void(0));
+    expect(s.lookupAsVar(X)).toBe(void(0));
+});
 
 test('simple lookup unbound', () => {
     const [X, sub] = Substitution.emptyPersistent<string>().freshVar();
